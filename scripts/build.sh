5ce91c14d57eff747dd60090397adfe39999a4dc
Mikael Sennerholm <mikael@sennerholm.net>
2017-01-12 18:06:23 +0100

    First version

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VERSION=${GO_PIPELINE_LABEL}
+
+set -e
+
+mkdir artifacts
+RUN curl -L -o /tmp/rancher.tgz https://github.com/rancher/cli/releases/download/v0.4.1/rancher-linux-amd64-v0.4.1.tar.gz && \
+	tar zxvf /tmp/rancher.tgz && \
+	mv rancher-*/rancher artifacts/rancher && \
+	rm -rf rancher-*
+# terraform 
+RUN curl -o /tmp/terraform.tgz https://releases.hashicorp.com/terraform/0.8.3/terraform_0.8.3_linux_amd64.zip && \
+  unzip /tmp/terraform.tgz && \
+  mv terraform /artifacts/terraform
