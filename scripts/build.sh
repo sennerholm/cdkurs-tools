249591573b02c5ac00bad75d5a640cce43a53b79
Mikael Sennerholm <mikael@sennerholm.net>
2017-01-12 18:24:25 +0100

    Fixed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,12 @@ VERSION=${GO_PIPELINE_LABEL}
 set -e
 
 mkdir artifacts
-curl -L -o /tmp/rancher.tgz https://github.com/rancher/cli/releases/download/v0.4.1/rancher-linux-amd64-v0.4.1.tar.gz && \
-	tar zxvf /tmp/rancher.tgz && \
+curl -L -o rancher.tgz https://github.com/rancher/cli/releases/download/v0.4.1/rancher-linux-amd64-v0.4.1.tar.gz && \
+	tar zxvf rancher.tgz && \
 	mv rancher-*/rancher artifacts/rancher && \
-	rm -rf rancher-*
+	rm -rf rancher-* rancher.tgz
 # terraform 
-curl -o /tmp/terraform.tgz https://releases.hashicorp.com/terraform/0.8.3/terraform_0.8.3_linux_amd64.zip && \
-  unzip /tmp/terraform.tgz && \
-  mv terraform artifacts/terraform
+curl -o terraform.zip https://releases.hashicorp.com/terraform/0.8.3/terraform_0.8.3_linux_amd64.zip && \
+  unzip terraform.zip && \
+  mv terraform artifacts/terraform && \
+  rm terraform.zip
